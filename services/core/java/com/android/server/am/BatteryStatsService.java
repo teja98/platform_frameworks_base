2132add9221d9ef26d0c2c3065cdb859749c0c9a
LineageOS-MTK <luzejij@binka.me>
2017-10-09 23:54:00 +0300

    Fix BatteryStatsService
    Исправляет ошибки:
    BatteryStatsService: Timeout reading modem stats
    BatteryStatsService: power: Missing API

--- services/core/java/com/android/server/am/BatteryStatsService.java
+++ services/core/java/com/android/server/am/BatteryStatsService.java
@@ -1450,7 +1450,6 @@ public final class BatteryStatsService extends IBatteryStats.Stub
     void updateExternalStatsSync(final String reason, int updateFlags) {
         SynchronousResultReceiver wifiReceiver = null;
         SynchronousResultReceiver bluetoothReceiver = null;
-        SynchronousResultReceiver modemReceiver = null;
 
         synchronized (mExternalStatsLock) {
             if (mContext == null) {
@@ -1486,11 +1485,6 @@ public final class BatteryStatsService extends IBatteryStats.Stub
                 if (mTelephony == null) {
                     mTelephony = TelephonyManager.from(mContext);
                 }
-
-                if (mTelephony != null) {
-                    modemReceiver = new SynchronousResultReceiver();
-                    mTelephony.requestModemActivityInfo(modemReceiver);
-                }
             }
 
             WifiActivityEnergyInfo wifiInfo = null;
@@ -1506,12 +1500,6 @@ public final class BatteryStatsService extends IBatteryStats.Stub
                 bluetoothInfo = awaitControllerInfo(bluetoothReceiver);
             } catch (TimeoutException e) {
                 Slog.w(TAG, "Timeout reading bt stats");
-            }
-
-            try {
-                modemInfo = awaitControllerInfo(modemReceiver);
-            } catch (TimeoutException e) {
-                Slog.w(TAG, "Timeout reading modem stats");
             }
 
             synchronized (mStats) {
